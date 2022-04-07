c0d6970a069fb40c381b570e0b5f079cc105dd06
VaRoXXas <kmichniewski@gmail.com>
2022-04-08 00:56:49 +0200

    Harpoon warning

--- Assets/Scripts/Harpoon.cs
+++ Assets/Scripts/Harpoon.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class Harpoon : MonoBehaviour
 {
+    public GameObject warning;
     private Movement whale;
     //private Vector3 whaleTransform;
     float spawnRange = 24.0f;
     Vector3 targetPosition;
     bool stop = false;
-    
+    bool canUpdate = false;
     [SerializeField] ParticleSystem Bubbles = null;
     ParticleSystem instantiatedBubbles;
     bool bubbleSpawned = false;
 
+    private GameObject spawnedWarning;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         float value = Random.Range(-spawnRange, spawnRange);
         targetPosition = new Vector3(value, 0, 0);
+
+        transform.LookAt(targetPosition);
+        Vector3 raySpawn = transform.position;
+        //raySpawn.y = raySpawn.y - 5;
+        spawnedWarning = Instantiate(warning, raySpawn, transform.rotation);
+        Invoke("StartUpdating", 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!stop)
+        if(!stop && canUpdate)
         {
-            transform.LookAt(targetPosition);
+            
+            if (spawnedWarning != null)
+            {
+                Destroy(spawnedWarning);
+            }
+            //transform.LookAt(targetPosition);
 
             Vector3 pos = Vector2.MoveTowards(transform.position, targetPosition, 55 * Time.deltaTime);
             transform.position = pos;
@@ -76,5 +91,10 @@ public class Harpoon : MonoBehaviour
     void SpawnBubbles()
     {
         instantiatedBubbles =  Instantiate(Bubbles, transform.position, Quaternion.identity);
+    }
+
+    void StartUpdating()
+    {
+        canUpdate = true;
     }
 }
