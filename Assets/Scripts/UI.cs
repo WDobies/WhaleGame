b18bfe25a136197f0e7b6f35c54d3801882814d9
WDobies <68401136+WDobies@users.noreply.github.com>
2022-06-22 17:00:01 +0200

    Instructions

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-public void LoadGame()
+
+    [SerializeField] GameObject MenuCanvas;
+    [SerializeField] GameObject Instructions;
+    [SerializeField] Button button;
+    public void LoadGame()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene(1);
+            
+         Time.timeScale = 1;
+         SceneManager.LoadScene(1);
+            
+    }
+
+    public void ShowInstructions()
+    {
+        MenuCanvas.SetActive(false);
+        Instructions.SetActive(true);
     }
 
     public void LoadMenu()
